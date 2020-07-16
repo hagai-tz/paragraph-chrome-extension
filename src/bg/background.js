ee380c7ccfa4a527f6ff9763f2b5c4d42e4b336d
hagai-tz <52886733+hagai-tz@users.noreply.github.com>
2020-07-16 17:06:45 +0300

    added options page, and support for default lang

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,13 +1,25 @@
 
 chrome.browserAction.onClicked.addListener(function (tab) {
-  console.log('this is tabs', tab);
+  console.log('this is tabs1', tab);
   // chrome.tabs.update(tab.id, {
   //   url: `http://localhost:3000/url?url=${tab.url}&lang=he`,
   //   // url: `https://paragraph-client.herokuapp.com/url?url=${tab.url}`
   // });
 
-  chrome.tabs.create({
-    // url: `http://localhost:3000/url?url=${tab.url}&lang=he`,
-    url: `https://www.paragraph.live/url?url=${tab.url}&lang=he`,
-  });
-});
+  chrome.storage.sync.get(['key'], function (result) {
+    console.log('Value currently is2 ' + result.key)
+    let userDefTarLang = result.key;
+
+    if (userDefTarLang === undefined) {
+      chrome.tabs.create({
+        url: `http://localhost:3000/url?url=${tab.url}&lang=DE`,
+        // url: `https://www.paragraph.live/url?url=${tab.url}&lang=DE`,
+      })
+    } else {
+      chrome.tabs.create({
+        url: `http://localhost:3000/url?url=${tab.url}&lang=${userDefTarLang}`,
+        // url: `https://www.paragraph.live/url?url=${tab.url}&lang=${userDefTarLang}`,
+      })
+    }
+  })
+})
