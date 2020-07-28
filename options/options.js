ac389daa65a0482dc8f15182f362a2bff9040870
hagai-tz <52886733+hagai-tz@users.noreply.github.com>
2020-07-28 22:06:09 +0300

    added support for languages and cleaned src langagu

--- options/options.js
+++ options/options.js
@@ -1,39 +1,165 @@
 
 /* toggle between hiding and showing the dropdown content */
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('dropdown-src').addEventListener('click', openSrc);
     document.getElementById('target-lang').addEventListener('click', openTar);
 }
 )
 
-function openSrc() {
-    //these functions open and close he menus and also do not allow them to be open together.
-    let openMenu = document.getElementById("target-lang")
-    if (openMenu.classList[1] === 'open')
-        document.getElementById("target-lang").classList.toggle("open");
-    document.getElementById("dropdown-src").classList.toggle("open");
-}
+chrome.storage.sync.get(['tarLang', 'tarLangName', 'tarLangImg'], function (result) {
+    if (result.tarLang === undefined) {
+        let x = result.tarLang
+        console.log("result.tarLang", x)
+        console.log("result", result)
+        let parent = document.getElementById('select-tar-lang')
+        let child = `<span id='select-tar-lang'>
+        <span style="color:blue; font-size:16px">Select Target Language</span>
+        <img src='./flags/arrow.png' alt='' class='arrow' />
+        </span>`
+        parent.innerHTML = ''
+        parent.insertAdjacentHTML('beforebegin', child)
+    }
+
+    else {
+        let parent = document.getElementById('select-tar-lang')
+        let child = `<span id='select-tar-lang'>
+        <img src=${result.tarLangImg} alt='' class='flags' />
+        <span>${result.tarLangName}</span>
+        <img src='./flags/arrow.png' alt='' class='arrow' />
+        </span>`
+        parent.innerHTML = ''
+        parent.insertAdjacentHTML('beforebegin', child)
+    }
+})
 
 function openTar() {
-    let openMenu = document.getElementById("dropdown-src")
-    if (openMenu.classList[1] === 'open')
-        document.getElementById("dropdown-src").classList.toggle("open");
     document.getElementById("target-lang").classList.toggle("open");
 }
 
 // Close the dropdown menu if the user clicks outside of it
 
 window.onclick = function (event) {
-    if (event.target.dataset.lang === 'src-lang') {
+    if (event.target.dataset.lang === 'tar-lang') {
         switch (event.target.dataset.id) {
+            case 'HI':
+                chrome.storage.sync.set({
+                    tarLang: event.target.dataset.id,
+                    tarLangName: event.target.dataset.langname,
+                    tarLangImg: event.target.dataset.img
+                }, function () {
+                    let parent = document.getElementById('select-tar-lang')
+                    let child = `<span id='select-tar-lang'>
+                             <img src=${event.target.dataset.img} alt='' class='flags' />
+                             <span>${event.target.dataset.langname}</span>
+                             <img src='./flags/arrow.png' alt='' class='arrow' />
+                         </span>`
+                    parent.innerHTML = ''
+                    parent.insertAdjacentHTML('beforebegin', child)
+                })
+                break;
+            case 'ZH-TW':
+                chrome.storage.sync.set({
+                    tarLang: event.target.dataset.id,
+                    tarLangName: event.target.dataset.langname,
+                    tarLangImg: event.target.dataset.img
+                }, function () {
+                    let parent = document.getElementById('select-tar-lang')
+                    let child = `<span id='select-tar-lang'>
+                             <img src=${event.target.dataset.img} alt='' class='flags' />
+                             <span>${event.target.dataset.langname}</span>
+                             <img src='./flags/arrow.png' alt='' class='arrow' />
+                         </span>`
+                    parent.innerHTML = ''
+                    parent.insertAdjacentHTML('beforebegin', child)
+                })
+                break;
+            case 'ZH-CN':
+                chrome.storage.sync.set({
+                    tarLang: event.target.dataset.id,
+                    tarLangName: event.target.dataset.langname,
+                    tarLangImg: event.target.dataset.img
+                }, function () {
+                    let parent = document.getElementById('select-tar-lang')
+                    let child = `<span id='select-tar-lang'>
+                             <img src=${event.target.dataset.img} alt='' class='flags' />
+                             <span>${event.target.dataset.langname}</span>
+                             <img src='./flags/arrow.png' alt='' class='arrow' />
+                         </span>`
+                    parent.innerHTML = ''
+                    parent.insertAdjacentHTML('beforebegin', child)
+                })
+                break;
+            case 'PL':
+                chrome.storage.sync.set({
+                    tarLang: event.target.dataset.id,
+                    tarLangName: event.target.dataset.langname,
+                    tarLangImg: event.target.dataset.img
+                }, function () {
+                    let parent = document.getElementById('select-tar-lang')
+                    let child = `<span id='select-tar-lang'>
+                             <img src=${event.target.dataset.img} alt='' class='flags' />
+                             <span>${event.target.dataset.langname}</span>
+                             <img src='./flags/arrow.png' alt='' class='arrow' />
+                         </span>`
+                    parent.innerHTML = ''
+                    parent.insertAdjacentHTML('beforebegin', child)
+                })
+                break;
+            case 'JA':
+                chrome.storage.sync.set({
+                    tarLang: event.target.dataset.id,
+                    tarLangName: event.target.dataset.langname,
+                    tarLangImg: event.target.dataset.img
+                }, function () {
+                    let parent = document.getElementById('select-tar-lang')
+                    let child = `<span id='select-tar-lang'>
+                             <img src=${event.target.dataset.img} alt='' class='flags' />
+                             <span>${event.target.dataset.langname}</span>
+                             <img src='./flags/arrow.png' alt='' class='arrow' />
+                         </span>`
+                    parent.innerHTML = ''
+                    parent.insertAdjacentHTML('beforebegin', child)
+                })
+                break;
+            case 'DE':
+                chrome.storage.sync.set({
+                    tarLang: event.target.dataset.id,
+                    tarLangName: event.target.dataset.langname,
+                    tarLangImg: event.target.dataset.img
+                }, function () {
+                    let parent = document.getElementById('select-tar-lang')
+                    let child = `<span id='select-tar-lang'>
+                             <img src=${event.target.dataset.img} alt='' class='flags' />
+                             <span>${event.target.dataset.langname}</span>
+                             <img src='./flags/arrow.png' alt='' class='arrow' />
+                         </span>`
+                    parent.innerHTML = ''
+                    parent.insertAdjacentHTML('beforebegin', child)
+                })
+                break;
+            case 'EN':
+                chrome.storage.sync.set({
+                    tarLang: event.target.dataset.id,
+                    tarLangName: event.target.dataset.langname,
+                    tarLangImg: event.target.dataset.img
+                }, function () {
+                    let parent = document.getElementById('select-tar-lang')
+                    let child = `<span id='select-tar-lang'>
+                             <img src=${event.target.dataset.img} alt='' class='flags' />
+                             <span>${event.target.dataset.langname}</span>
+                             <img src='./flags/arrow.png' alt='' class='arrow' />
+                         </span>`
+                    parent.innerHTML = ''
+                    parent.insertAdjacentHTML('beforebegin', child)
+                })
+                break;
             case 'ES':
                 chrome.storage.sync.set({
-                    srcLang: event.target.dataset.id,
-                    srcLangName: event.target.dataset.langname,
-                    srcLangImg: event.target.dataset.img
+                    tarLang: event.target.dataset.id,
+                    tarLangName: event.target.dataset.langname,
+                    tarLangImg: event.target.dataset.img
                 }, function () {
-                    let parent = document.getElementById('select-src-lang')
-                    let child = `<span id='select-src-lang'>
+                    let parent = document.getElementById('select-tar-lang')
+                    let child = `<span id='select-tar-lang'>
                              <img src=${event.target.dataset.img} alt='' class='flags' />
                              <span>${event.target.dataset.langname}</span>
                              <img src='./flags/arrow.png' alt='' class='arrow' />
@@ -45,94 +171,6 @@ window.onclick = function (event) {
 
             case 'HE':
                 chrome.storage.sync.set({
-                    srcLang: event.target.dataset.id,
-                    srcLangName: event.target.dataset.langname,
-                    srcLangImg: event.target.dataset.img
-                }, function () {
-                    let parent = document.getElementById('select-src-lang')
-                    let child = `<span id='select-src-lang'>
-                             <img src=${event.target.dataset.img} alt='' class='flags' />
-                             <span>${event.target.dataset.langname}</span>
-                             <img src='./flags/arrow.png' alt='' class='arrow' />
-                         </span>`
-                    parent.innerHTML = ''
-                    parent.insertAdjacentHTML('beforebegin', child)
-                })
-                break;
-
-            case 'AR':
-                chrome.storage.sync.set({
-                    srcLang: event.target.dataset.id,
-                    srcLangName: event.target.dataset.langname,
-                    srcLangImg: event.target.dataset.img
-                }, function () {
-                    let parent = document.getElementById('select-src-lang')
-                    let child = `<span id='select-src-lang'>
-                             <img src=${event.target.dataset.img} alt='' class='flags' />
-                             <span>${event.target.dataset.langname}</span>
-                             <img src='./flags/arrow.png' alt='' class='arrow' />
-                         </span>`
-                    parent.innerHTML = ''
-                    parent.insertAdjacentHTML('beforebegin', child)
-                })
-                break;
-
-            case 'RU':
-                chrome.storage.sync.set({
-                    srcLang: event.target.dataset.id,
-                    srcLangName: event.target.dataset.langname,
-                    srcLangImg: event.target.dataset.img
-                }, function () {
-                    let parent = document.getElementById('select-src-lang')
-                    let child = `<span id='select-src-lang'>
-                             <img src=${event.target.dataset.img} alt='' class='flags' />
-                             <span>${event.target.dataset.langname}</span>
-                             <img src='./flags/arrow.png' alt='' class='arrow' />
-                         </span>`
-                    parent.innerHTML = ''
-                    parent.insertAdjacentHTML('beforebegin', child)
-                })
-                break;
-
-            case 'FR':
-                chrome.storage.sync.set({
-                    srcLang: event.target.dataset.id,
-                    srcLangName: event.target.dataset.langname,
-                    srcLangImg: event.target.dataset.img
-                }, function () {
-                    let parent = document.getElementById('select-src-lang')
-                    let child = `<span id='select-src-lang'>
-                             <img src=${event.target.dataset.img} alt='' class='flags' />
-                             <span>${event.target.dataset.langname}</span>
-                             <img src='./flags/arrow.png' alt='' class='arrow' />
-                         </span>`
-                    parent.innerHTML = ''
-                    parent.insertAdjacentHTML('beforebegin', child)
-                })
-                break;
-        }
-
-    } else if (event.target.dataset.lang === 'tar-lang') {
-        switch (event.target.dataset.id) {
-            case 'ES':
-                chrome.storage.sync.set({
-                    tarLang: event.target.dataset.id,
-                    tarLangName: event.target.dataset.langname,
-                    tarLangImg: event.target.dataset.img
-                }, function () {
-                    let parent = document.getElementById('select-tar-lang')
-                    let child = `<span id='select-tar-lang'>
-                             <img src=${event.target.dataset.img} alt='' class='flags' />
-                             <span>${event.target.dataset.langname}</span>
-                             <img src='./flags/arrow.png' alt='' class='arrow' />
-                         </span>`
-                    parent.innerHTML = ''
-                    parent.insertAdjacentHTML('beforebegin', child)
-                })
-                break;
-
-            case 'HE':
-                chrome.storage.sync.set({
                     tarLang: event.target.dataset.id,
                     tarLangName: event.target.dataset.langname,
                     tarLangImg: event.target.dataset.img
@@ -198,7 +236,6 @@ window.onclick = function (event) {
                     parent.insertAdjacentHTML('beforebegin', child)
                 })
                 break;
-
         }
 
 
--- src/bg/background.js
+++ src/bg/background.js
@@ -11,24 +11,11 @@ _gaq.push(['_setAccount', 'UA-159876106-1']);
 chrome.browserAction.onClicked.addListener(function (tab) {
   console.log('this is tabs', tab);
   _gaq.push(['_trackEvent', 'exClick', 'click', tab.url]);
-  
-  // chrome.storage.sync.clear()
-  chrome.storage.sync.get(['srcLang', 'tarLang'], function (result) {
-    
-    let userDefSrcLang = result.srcLang;
-    let userDefTarLang = result.tarLang;
-    console.log('userDefSrcLang', userDefSrcLang);
-    console.log('userDefTarLang', userDefTarLang);
 
-    if (userDefSrcLang === undefined) {
-      if (typeof document.documentElement.lang === 'undefined') {
-        userDefSrcLang = document.documentElement.lang.toLocaleUpperCase()
-        _gaq.push(['_trackEvent', 'userLangEx', userDefSrcLang]); //sends the user default target lang
-      } else {
-        userDefSrcLang = document.documentElement.lang.toLocaleUpperCase()
-        _gaq.push(['_trackEvent', 'userLangEx', userDefSrcLang]); //sends the user default target lang
-      }
-    }
+  chrome.storage.sync.get(['tarLang'], function (result) {
+
+    let userDefTarLang = result.tarLang;
+    console.log('userDefTarLang', userDefTarLang);
 
     if (userDefTarLang === undefined) {
       if (typeof window.navigator.languages[1] === 'undefined') {
@@ -42,31 +29,8 @@ chrome.browserAction.onClicked.addListener(function (tab) {
 
 
     chrome.tabs.create({
-      url: `http://localhost:3000/url?url=${tab.url}&srcLang=${userDefSrcLang}&tarLang=${userDefTarLang}`,
-      // url: `https://www.paragraph.live/url?url=${tab.url}&srcLang=${userDefSrcLang}&tarLang=${userDefTarLang}`,
+      // url: `http://localhost:3000/url?url=${tab.url}&tarLang=${userDefTarLang}`,
+      url: `https://www.paragraph.live/url?url=${tab.url}tarLang=${userDefTarLang}`,
     })
   })
 })
-
-// if (userDefTarLang === undefined) {
-//   if (typeof window.navigator.languages[1] === 'undefined') {
-//     userDefTarLang = window.navigator.languages[0].slice(0, 2).toLocaleUpperCase()
-//     _gaq.push(['_trackEvent', 'userLangEx', userDefTarLang]); //sends the user default target lang
-//   } else {
-//     userDefTarLang = window.navigator.languages[1].slice(0, 2).toLocaleUpperCase()
-//     _gaq.push(['_trackEvent', 'userLangEx', userDefTarLang]); //sends the user default target lang
-//   }
-
-
-//   chrome.tabs.create({
-//     url: `http://localhost:3000/url?url=${tab.url}&srcLang=${userDefSrcLang}&tarLang=${userDefTarLang}`
-//     // url: `https://www.paragraph.live/url?url=${tab.url}&srcLang=${userDefSrcLang}&tarLang=${userDefTarLang}`,
-//   })
-// }
-
-// else {
-//   chrome.tabs.create({
-//     url: `http://localhost:3000/url?url=${tab.url}&srcLang=${userDefSrcLang}&tarLang=${userDefTarLang}`,
-//     // url: `https://www.paragraph.live/url?url=${tab.url}&srcLang=${userDefSrcLang}&tarLang=${userDefTarLang}`,
-//   })
-// }
